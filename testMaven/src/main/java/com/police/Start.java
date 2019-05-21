3c32012419eae7427b2c16cb705cfaa5ddf72aeb
Zhang Sensen <zhangsensen_vendor@domain.sensetime.com>
2019-05-21 11:18:56 +0800

    first

---
+++ testMaven/src/main/java/com/police/Start.java
@@ -0,0 +1,10 @@
+package com.police;
+
+/**
+ * Created by zhangsensen_vendor on 2019/5/21.
+ */
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("TestM");
+    }
+}
